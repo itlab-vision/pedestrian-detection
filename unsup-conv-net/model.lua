349f82c0d040bafa1894c424b3020afc06384db7
KruchDmitriy <kruch.dmitriy@gmail.com>
2015-09-29 10:18:23 +0300

    updated model

--- unsup-conv-net/model.lua
+++ unsup-conv-net/model.lua
@@ -2,51 +2,52 @@ require "nn"
 require "image"
 
 num_conv = 2
+gauss_kernel = image.gaussian(9, 0.25, 1, true)
 
 model = nn.Sequential()
 
 -- 1st layer
-model:add(nn.SplitTable(1))
+--model:add(nn.SplitTable(1))
+    par = nn.ParallelTable()
 
-par = nn.ParallelTable()
+        func_y = nn.Sequential()
+        func_y:add(nn.SpatialConvolutionMap(nn.tables.full(1, 32), 7, 7, 1, 1))
+        func_y:add(nn.ReLU())
+        func_y:add(nn.SpatialContrastiveNormalization(32, gauss_kernel))
+        func_y:add(nn.SpatialAveragePooling(3, 3, 3, 3))
 
-func_y = nn.Sequential()
-func_y:add(nn.SpatialConvolution(1, 32, 7, 7))
-func_y:add(nn.ReLU())
-func_y:add(image.lcn())
-func_y:add(nn.SpatialSubSampling(32, 3, 3, 3, 3))
+        func_uv = nn.Sequential()
+        func_uv:add(nn.SpatialAveragePooling(3, 3, 3, 3))
+        func_uv:add(nn.SpatialConvolutionMap(nn.tables.full(2, 6), 3, 3))
+        func_uv:add(nn.ReLU())
+        func_uv:add(nn.SpatialContrastiveNormalization(6, gauss_kernel))
 
-func_uv = nn.Sequential()
-func_uv:add(nn.SpatialSubSampling(1, 3, 3, 3, 3))
-func_uv:add(nn.SpatialConvolution(1, 6, 5, 5))
-func_uv:add(nn.ReLU())
-func_uv:add(image.lcn())
-
-par:add(func_y)
-par:add(func_uv)
-par:add(func_uv)
+    par:add(func_y)
+    par:add(func_uv)
 
 model:add(par)
-
-model:add(nn.JointTable(1))
+model:add(nn.JoinTable(1))
 
 
 -- 2nd layer
-table = nn.tables.random(38, 68, 2040)
-scm = nn.SpatialConvolutionMap(table, 9, 9)
-model:add(scm)
-func:add(nn.ReLU())
-func:add(image.lcn())
-func:add(nn.SpatialSubSampling(68, 2, 2, 2, 2))
+model:add(nn.SpatialConvolutionMap(nn.tables.random(38, 68, 30), 9, 9))
+    func = nn.Sequential()
+    func:add(nn.ReLU())
+    func:add(nn.SpatialContrastiveNormalization(68, gauss_kernel))
+    func:add(nn.SpatialAveragePooling(2, 2, 2, 2))
 model:add(func)
 
+
 -- 3rd layer
-model:add(nn.Reshape(17824))
+model:add(nn.View(17824))
 model:add(nn.Linear(17824, 2))
 model:add(nn.LogSoftMax())
 
 -- loss
 loss = nn.ClassNLLCriterion()
 
+print(model)
+
+print(model:forward{torch.Tensor(1, 126, 78), torch.Tensor(2, 126, 78)})--:size())
 
 return model, num_conv
